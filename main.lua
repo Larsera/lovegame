2ce9e250e23f06bcf34a397bbba80b0aad04da04
Lars Erik realfsen <larsera@gmail.com>
2013-10-05 22:34:36 +0200

    Added bounding boxes to screen edges

--- main.lua
+++ main.lua
@@ -7,8 +7,8 @@ function love.load()
         player_x = 0
         velY = 0
         velX = 0
-        speedlimit = 2
-        friction = 0.45
+        speedlimit = 5
+        friction = 0.96
 
 end
 
@@ -34,14 +34,35 @@ function love.update(dt)
                         velX = velX - 1
                 end
         end
+
         velY = velY * friction
         player_y = player_y + velY
         velX = velX * friction
         player_x = player_x + velX
+
+        if player_y + 64 >= lg.getHeight() then
+                player_y = lg.getHeight() -64
+                velY = -velY
+        end
+        if player_x + 64 >= lg.getWidth() then
+                player_x = lg.getWidth() - 64
+                velX = -velX
+        end
+
+        if player_y <= 0 then 
+                player_y = 0
+                velY = -velY
+        end
+        if player_x <= 0 then
+                player_x = 0
+                velX = -velX
+        end
 end
 
 function love.draw()
         love.graphics.draw(background, 0, 0)
         lg.draw(player, player_x, player_y)
+        lg.print("X: " .. player_x, 100, 100)
+        lg.print("Y: " .. player_y, 100, 120)
 end
 
